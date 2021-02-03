32bb895a099c93f583f95fc77a3d57ac3cd9af0d
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-02-03 15:32:41 +0100

    add resultstable scripts

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ const editBtn = document.querySelector('.edit-name');
 const startBtn = document.querySelector('.play');
 const result = document.querySelector('.header h1');
 
+const resultName1 = document.querySelector('.one');
+const resultName2 = document.querySelector('.two');
+const results1 = document.querySelector('.results1');
+const results2 = document.querySelector('.results2');
+const results3 = document.querySelector('.results3');
+let wins1 = 0;
+let wins2 = 0;
+let draws = 0;
 
 const editNames = () => {
     const newNameOne = prompt('Set name for Player One', 'Player One');
     playerOneName.textContent = newNameOne;
+    resultName1.textContent = newNameOne;
     const newNameTwo = prompt('Set name for Player Two', 'Player Two');
     playerTwoName.textContent = newNameTwo;
+    resultName2.textContent = newNameTwo;
 };
 
 const startGame = () => {
@@ -29,12 +39,22 @@ const startGame = () => {
 
         if (numberDiceOne > numberDiceTwo) {
             result.textContent = `${playerOneName.textContent} wins!`
+            wins1++;
+            results1.innerHTML = wins1;
         } else if (numberDiceTwo > numberDiceOne) {
             result.textContent = `${playerTwoName.textContent} wins!`
+            wins2++;
+            results2.innerHTML = wins2;
         } else {
             result.textContent = 'Draw!'
+            draws++;
+            results3.textContent = draws;
         };
     }, 1500);
+};
+
+const resultsTable = () => {
+
 };
 
 
